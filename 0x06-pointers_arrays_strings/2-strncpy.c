d576c710a321d4b9d2642f4de656ceedf881bc72
dedozer464 <doza393@gmail.com>
2022-09-23 04:44:53 -0700

    pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+int i;
 
     /**
      * iterate through src array
@@ -19,8 +19,8 @@ char *_strncpy(char *dest, char *src, int n)
      * the string placed in dest will not be
      * null terminated
     */
-    for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[i] = src[i];
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
     /**
      * if the length of source is less than n
      * write additional nullbytes to dest to
