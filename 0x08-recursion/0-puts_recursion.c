d82159ef131efc9f3433a8d324def5d24f983568
dedozer464 <doza393@gmail.com>
2022-09-28 23:41:11 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,7 +6,7 @@
   * Return: no return. 
   */ 
 void _puts_recursion(char *s) 
- { 
+{ 
          if (*s != '\0') 
          { 
                  _putchar(*s); 
@@ -14,4 +14,4 @@ void _puts_recursion(char *s)
          } 
          else 
                  _putchar('\n'); 
- }
+}
